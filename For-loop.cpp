63c2905de7302ebdd2d6f88ac348eadf8bd52d35
Vikrant Kumar Verma <103443567+616Vikrant@users.noreply.github.com>
2023-08-23 12:11:07 +0530

    For loop introduced

---
+++ For-loop.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter value of n:"<<endl;
+    cin>>n;
+    cout<<"Numbers from 1 to "<<n<<" are: ";
+    for (int i=1;i<=n;i++){
+        cout<<i<<", ";
+    }
+} 
