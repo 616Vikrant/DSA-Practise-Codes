031acdeef3539a06411c69647ae934c03942e02a
Vikrant Kumar Verma <103443567+616Vikrant@users.noreply.github.com>
2023-08-24 16:10:20 +0530

    for loop (general questions)

--- For-loop.cpp
+++ For-loop.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+/* #include <iostream>
 using namespace std;
 int main(){
     int n;
@@ -8,4 +8,52 @@ int main(){
     for (int i=1;i<=n;i++){
         cout<<i<<", ";
     }
-} 
+}  */
+/* #include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter value of n:"<<endl;
+    cin>>n;
+    int sum=0;
+    for (int i=1;i<=n;i++){
+        sum=sum+i;
+    }
+    cout<<"sum is: "<<sum;
+} */
+//fionacci series
+/* #include <iostream>
+using namespace std;
+int main(){
+    int n; 
+    cout<<"enter value of n:";
+    cin>>n;
+    int a=0;
+    int b=1;
+    cout<<a<<" "<<b<<" ";
+    for (int i=1;i<=n;i++){
+        int next=a+b;
+        cout<<next<<" ";
+        a=b;
+        b=next;
+    }
+} */
+//prime or not
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter n:";
+    cin>>n;
+    bool isprime=1;
+    for (int i=2;i<n;i++){
+        if(n%i==0){
+            isprime=0;
+            break;
+            }
+    }
+    if (isprime==0){
+        cout<<"Not a prime number";
+    }
+    else {cout<<"Prime number";}
+}
