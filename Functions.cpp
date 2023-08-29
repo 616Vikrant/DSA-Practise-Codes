38e1541e12e0829ef13fe4cfb352ceae0c887f80
Vikrant Kumar Verma <103443567+616Vikrant@users.noreply.github.com>
2023-08-30 01:38:51 +0530

    functions

--- Functions.cpp
+++ Functions.cpp
@@ -20,7 +20,7 @@ int main(){
     cout<<power(a,b);
     return 0;
 } */
-//function to check even no. and return in bool
+/* //function to check even no. and return in bool
 # include<iostream>
 using namespace std;
 bool iseven(int a){
@@ -35,5 +35,57 @@ int main(){
     int a;
     cout<<"enter value of a:";
     cin>>a;
-    cout<<iseven(a);
+    if (iseven(a)){
+        cout<<"Even number";
+    }
+    else{
+        cout<<"Odd number";
+    }
+} */
+/* //function for nCr
+#include<iostream>
+using namespace std;
+int factorial(int n)
+{
+    int res = 1, i;
+    for (i = 2; i <= n; i++)
+        res *= i;
+    return res;
+}
+int combination(int n,int r){
+    int ans1=factorial(n);
+    int ans2=factorial(n-r);
+    int ans3=factorial(r);
+    int answer=ans1/(ans3*ans2);
+    return answer;
+}
+int main(){
+    int n,r;
+    cout<<"enter value of n:";
+    cin>>n;
+    cout<<"enter value of r:";
+    cin>>r;
+    cout<<(combination(n,r));
+} */
+//is prime or not return bool
+#include<iostream>
+using namespace std;
+bool isPrime(int n){
+    for(int i=2;i<n;i++){
+        if(n%i==0){
+            return false;
+        }
+    return true;
+    }
+}
+int main(){
+    int n;
+    cout<<"enter value of n:";
+    cin>>n;
+    if(isPrime(n)){
+        cout<<n<<" is Prime";
+    }
+    else{
+        cout<<n<<" is not Prime";
+    }
 }
