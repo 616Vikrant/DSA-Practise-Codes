45b313adfa1dfe6088531c79ba92cf200fa4a0c7
Vikrant Kumar Verma <103443567+616Vikrant@users.noreply.github.com>
2023-08-29 23:50:25 +0530

    functions

--- Functions.cpp
+++ Functions.cpp
@@ -1,4 +1,4 @@
-//power function
+/* //power function
 #include<iostream>
 using namespace std;
 int power (int a , int b){
@@ -19,4 +19,21 @@ int main(){
     //int answer = power(a,b);
     cout<<power(a,b);
     return 0;
+} */
+//function to check even no. and return in bool
+# include<iostream>
+using namespace std;
+bool iseven(int a){
+    if(a&1){
+        return false;
+        }
+    else{
+        return true;
+        }
+    }
+int main(){
+    int a;
+    cout<<"enter value of a:";
+    cin>>a;
+    cout<<iseven(a);
 }
