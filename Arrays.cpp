a3cf3d512b5a9da871e8e587dd6a886885a975f0
Vikrant Kumar Verma <103443567+616Vikrant@users.noreply.github.com>
2023-09-05 22:28:29 +0530

    linear search

--- Arrays.cpp
+++ Arrays.cpp
@@ -1,5 +1,5 @@
 //Find sum of all elements of an array;
-#include <iostream>
+/* #include <iostream>
 using namespace std;
 int sum(int arr[],int size){
     int count=0;
@@ -18,4 +18,92 @@ int main(){
         cin>>arr[i];
     }
     cout<<"sum is : "<<sum(arr,size);
+} */
+
+//Linear Search
+/* #include<iostream>
+using namespace std;
+bool Linearsearch(int array[],int size,int key){
+    for (int i=0;i<size;i++){
+        if (array[i]==key){
+            return true;
+        }
+    }
+    return 0;
+}
+int main(){
+    int array[10]={2,65,7,756,455,34,223,12,3,56};
+    int key;
+    cout<<"enter key element:";
+    cin>> key;
+    bool found = Linearsearch(array,10,key);
+    if (found){
+        cout<<"Yes "<<key<<" is present.";
+    }
+    else {
+        cout<<"No "<<key<<" is not present.";
+    }
+} */
+
+
+//Reverse an array
+/* #include<iostream>
+using namespace std;
+
+void reverse(int array[],int size){
+    int start=0;
+    int end=size-1;
+    while(start<=end){
+        swap(array[start],array[end]);
+        start++;
+        end--;
+    }
+}
+
+void printarray(int array[],int size){
+    for(int i=0;i<size;i++){
+        cout<<array[i]<<" ";
+    }
+    cout<<endl;
+}
+int main(){
+    int size;
+    cout<<"enter size of array:";
+    cin>>size;
+    int array[size];
+    cout<<endl<<"enter elements of array:";
+    for (int i=0;i<size;i++){
+        cin>>array[i];
+    }
+    reverse(array,size);
+    printarray(array,size);
+} */
+
+//reverse adjacent element in an array;
+#include<iostream>
+using namespace std;
+
+void reverseadjacent(int array[],int size){
+    for(int i=0;i<size;i=i+2){
+        swap(array[i],array[i+1]);
+    }
+}
+
+void printarray(int array[],int size){
+    for(int i=0;i<size;i++){
+        cout<<array[i]<<" ";
+    }
+    cout<<endl;
+}
+int main(){
+    int size;
+    cout<<"enter size of array:";
+    cin>>size;
+    int array[size];
+    cout<<endl<<"enter elements of array:";
+    for (int i=0;i<size;i++){
+        cin>>array[i];
+    }
+    reverseadjacent(array,size);
+    printarray(array,size);
 }
